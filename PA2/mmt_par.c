8985fa741793b2fb88f3f668f567d3f3935bc368
Randy1005 <randychang.10@gmail.com>
2022-11-04 13:32:30 -0600

    unroll degree changed

--- PA2/mmt_par.c
+++ PA2/mmt_par.c
@@ -2,7 +2,7 @@ void mmt_par(int n, double *__restrict__ a, double *__restrict__ b,
                  double *__restrict__ c) {
 int i, j, k;
 
-int remainder = n % 3;
+int remainder = n % 4;
 
 #pragma omp parallel private(i,j,k) 
 {
@@ -14,11 +14,12 @@ int remainder = n % 3;
 				c[i*n+j]=c[i*n+j]+a[k*n+j]*b[k*n+i];
 			}
 
-			for(j = remainder; j < n; j += 3) {
+			for(j = remainder; j < n; j += 4) {
 //    c[i][j] = c[i][j] + a[k][j]*b[k][i];
 				c[i*n+j]=c[i*n+j]+a[k*n+j]*b[k*n+i];
 				c[i*n+(j + 1)]=c[i*n+ (j + 1)]+a[k*n+ (j + 1)]*b[k*n+i];
 				c[i*n+ (j + 2)]=c[i*n+ (j + 2)]+a[k*n+(j + 2)]*b[k*n+i];
+				c[i*n+ (j + 3)]=c[i*n+ (j + 3)]+a[k*n+(j + 3)]*b[k*n+i];
 			}
 		}
 
