361d99f2c51132674b493213bc0b7cd207dcd298
Randy1005 <randychang.10@gmail.com>
2022-11-04 15:53:52 -0600

    modified simd

--- PA2/mv2_par.c
+++ PA2/mv2_par.c
@@ -2,17 +2,31 @@
 void mv2_par(int n, double *__restrict__ m, double *__restrict__ x, double *__restrict__ y, double *__restrict__ z)
 {
   int i, j;
+	int rem = n % 4;
 #pragma omp parallel private(i,j)
 {
-	#pragma omp for schedule(static)
-  for (j = 0; j < n; j++)
+	#pragma omp for
+  for (j = 0; j < n; j++) {
 		#pragma simd
-		for (i = 0; i < n; i++) {
+		for (i = 0; i < rem; i++) {
 //    y[j] = y[j] + m[i][j] * x[i];
 //    z[j] = z[j] + m[j][i] * x[i];
       y[j] = y[j] + m[i*n+j] * x[i];
       z[j] = z[j] + m[j*n+i] * x[i];
 		}
+
+		#pragma simd
+		for (i = rem; i < n; i+=4) {
+      y[j] = y[j] + m[i*n+j] * x[i];
+      z[j] = z[j] + m[j*n+i] * x[i];
+      y[j] = y[j] + m[(i + 1)*n+j] * x[i + 1];
+      z[j] = z[j] + m[j*n+(i + 1)] * x[i + 1];
+      y[j] = y[j] + m[(i + 2)*n+j] * x[i + 2];
+      z[j] = z[j] + m[j*n+(i + 2)] * x[i + 2];
+      y[j] = y[j] + m[(i + 3)*n+j] * x[i + 3];
+      z[j] = z[j] + m[j*n+(i + 3)] * x[i + 3];
+		}
+	}
 
 }
 
