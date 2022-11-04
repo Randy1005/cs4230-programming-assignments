c169cf6b85342b5ea74429a06804d68d62fb7269
Randy1005 <randychang.10@gmail.com>
2022-11-04 12:14:35 -0600

    parallel for on i loop

--- PA2/mmt_par.c
+++ PA2/mmt_par.c
@@ -4,8 +4,8 @@ int i, j, k;
 
 #pragma omp parallel private(i,j,k) 
 {
-#pragma omp master
 	for(k = 0; k < n; k++)
+	#pragma omp for
 		for(i=0;i<n;i++)
 			for(j=0;j<n;j++)
 //    c[i][j] = c[i][j] + a[k][j]*b[k][i];
